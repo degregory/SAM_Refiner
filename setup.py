bffe49bbe2e7a925bdadd96cf1796cb9035456d6
degregory <virologistd@gmail.com>
2021-06-09 09:12:37 -0500

    files for dist

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(  name='SAM_Refiner',
+        version='1.0',
+        py_modules=['SAM_Refiner'],
+        description='A program for gathering variant information from a SAM formated files',
+        author='Devon A. Gregory',
+        author_email='gregoryde@missourie.edu',
+        url='https://github.com/degregory/SAM_Refiner',
+        license='GPL-3.0'
+      )
