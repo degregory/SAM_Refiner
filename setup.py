78bcadb84404b4bc2f05ee1a0dc43566fcdf4f54
degregory <virologistd@gmail.com>
2021-06-11 14:14:13 -0500

    mod for pip install

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(  name='SAM_Refiner',
         version='1.0',
         py_modules=['SAM_Refiner'],
         description='A program for gathering variant information from a SAM formated files',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
         author='Devon A. Gregory',
         author_email='gregoryde@missourie.edu',
         url='https://github.com/degregory/SAM_Refiner',
-        license='GPL-3.0'
+        license='GPL-3.0',
+        entry_points={
+            'console_scripts': [
+                'SAM_Refiner = SAM_Refiner:main',
+            ],
+        },
       )
